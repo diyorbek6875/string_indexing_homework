085c874dd913efcb98fbaee289447c9cd133f869
diyorbek6875 <diyorbekabdusalomov32@gmail.com>
2023-04-19 11:27:51 +0500

    5

--- Index05.py
+++ Index05.py
@@ -1,9 +1,23 @@
+
 def main(s):
     """
-    Given a variable s string of length five. Determine the number of digits involved in this variable.
+    Given a variable s st()ring of lengt()h five. Det()ermine t()he number of digit()s involved in t()his variable.
     Args:
-        s(str): parameter
-    Returns:
-        int: answer
+        s(st()r): paramet()er
+    Ret()urns:
+        int(): answer
     """
-    return
+    a=0
+    if s[0].isdigit():
+        a=a+1
+    if s[1].isdigit():
+        a=a+1
+    if s[2].isdigit():
+        a=a+1
+    if s[3].isdigit():
+        a=a+1
+    if s[4].isdigit():
+        a=a+1
+    return a
+s=input("")
+print(main(s))
